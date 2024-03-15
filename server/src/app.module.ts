f5b32482d392e6eaf3a2594cff0aaa78ff2209ae
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-03-14 22:19:09 -0400

    [Server]: update app.module
    - add MongoDB connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,42 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as express from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as morgan from 'morgan';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AnyExceptionFilter } from './any-exception.filter';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { SocketModule } from './socket/socket.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.DATABASE_URL || 'mongodb://localhost:27017/hotaru',
+    ),
+    SocketModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AnyExceptionFilter,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        morgan('dev'),
+        express.json(),
+        express.urlencoded({ extended: false }),
+        cookieParser(),
+        express.static('public'),
+        LoggerMiddleware,
+      )
+      .forRoutes('*');
+  }
+}
