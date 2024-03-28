0a4290be72664020b99391d7ecbda0d8a7477714
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-03-28 13:52:31 -0400

    [Server]: update UserSchema

--- server/src/users/user.schema.ts
+++ server/src/users/user.schema.ts
@@ -12,8 +12,20 @@ export class User {
   @Prop({ required: true, unique: true })
   username: string
 
+  @Prop({ required: false, unique: true })
+  emailAddress: string
+
   @Prop({ required: true })
   password: string
+
+  @Prop({ required: false })
+  birthYear: string
+
+  @Prop({ required: false })
+  birthMonth: string
+
+  @Prop({ required: false })
+  birthDay: string
 }
 
 // Create a schema for the User class.
