27154ea558b09a253b3242cb07dff1e56a35804b
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-03-28 13:52:07 -0400

    [Server]: rename and improve register-user-dto

---
+++ server/src/users/dto/register-user.dto.ts
@@ -0,0 +1,26 @@
+import { IsEmail, IsString, MinLength } from 'class-validator'
+
+/**
+ * Data transfer object for creating a new user.
+ */
+export class RegisterUserDto {
+  @IsEmail()
+  emailAddress: string
+
+  @IsString()
+  @MinLength(6)
+  username: string
+
+  @IsString()
+  @MinLength(8)
+  password: string
+
+  @IsString()
+  birthYear: string
+
+  @IsString()
+  birthMonth: string
+
+  @IsString()
+  birthDay: string
+}
