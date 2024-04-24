6e407bf61a1383eb922001f5465890bf70bf7675
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-04-24 19:12:56 -0400

    [Server] Fix middleware

--- server/src/common/middleware/logger.middleware.ts
+++ server/src/common/middleware/logger.middleware.ts
@@ -1,12 +1,12 @@
 import { Injectable, NestMiddleware } from '@nestjs/common'
-import { Request, NextFunction } from 'express'
+import { Request, Response, NextFunction } from 'express'
 
 /**
  * Middleware that logs the request method and URL to the console.
  */
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, next: NextFunction) {
+  use(req: Request, res: Response, next: NextFunction) {
     console.log('Request...', req.method, req.originalUrl)
     next()
   }
