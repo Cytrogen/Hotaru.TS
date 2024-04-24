5497f357de4a9bf2587b14278b31369a47350961
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-04-24 19:13:30 -0400

    [Server] Provide UsersService to AuthModule

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
 import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
+import { APP_GUARD } from '@nestjs/core'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { User, UserSchema } from '../users/user.schema'
+import { UsersService } from '../users/users.service'
 import jwtConfig from '../common/config/jwt.config'
+import { AccessTokenGuard } from './guards/access-token.guard'
 
 @Module({
   imports: [
@@ -19,6 +22,13 @@ import jwtConfig from '../common/config/jwt.config'
     ]),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
+    },
+    UsersService,
+  ],
 })
 export class AuthModule {}
