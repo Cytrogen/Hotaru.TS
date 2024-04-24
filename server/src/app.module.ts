3063ff2420e5e1f536c8e6d0a59d8cfcc1e96edb
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-04-24 03:31:54 -0400

    [Server] Add ConfigModule for JWT configurations

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common'
 import { APP_FILTER } from '@nestjs/core'
+import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 import * as express from 'express'
 import * as cookieParser from 'cookie-parser'
 import * as morgan from 'morgan'
+import * as Joi from 'joi'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AnyExceptionFilter } from './any-exception.filter'
@@ -13,6 +15,14 @@ import { UsersModule } from './users/users.module'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        JWT_TOKEN_AUDIENCE: Joi.string().required(),
+        JWT_TOKEN_ISSUER: Joi.string().required(),
+        JWT_ACCESS_TOKEN_TTL: Joi.number().default(3600),
+      }),
+    }),
     MongooseModule.forRoot(process.env.DATABASE_URL || 'mongodb://localhost:27017/hotaru'),
     SocketModule,
     UsersModule,
