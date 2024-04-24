b2763542a6365ce279d69f3f72c9c9004208f16f
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-04-24 03:34:21 -0400

    [Server] Add comments

--- server/src/common/middleware/logger.middleware.ts
+++ server/src/common/middleware/logger.middleware.ts
@@ -1,10 +1,13 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
+import { Injectable, NestMiddleware } from '@nestjs/common'
+import { Request, NextFunction } from 'express'
 
+/**
+ * Middleware that logs the request method and URL to the console.
+ */
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request...', req.method, req.originalUrl);
-    next();
+  use(req: Request, next: NextFunction) {
+    console.log('Request...', req.method, req.originalUrl)
+    next()
   }
 }
