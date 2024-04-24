b6f24d031fea805ec92792e5df3243756a0977e7
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-04-24 19:10:50 -0400

    [Server] Send exception message to client instead of static words

--- server/src/any-exception.filter.ts
+++ server/src/any-exception.filter.ts
@@ -1,15 +1,18 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common'
+import { Request, Response } from 'express'
 
 @Catch()
 export class AnyExceptionFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp()
-    const response = ctx.getResponse()
-    const request = ctx.getRequest()
+    const response = ctx.getResponse<Response>()
+    const request = ctx.getRequest<Request>()
 
     const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR
 
-    const message = exception instanceof HttpException ? exception.getResponse() : 'Internal server error'
+    const message = exception instanceof HttpException ? exception.getResponse() : exception
+
+    console.error(exception)
 
     response.status(status).json({
       statusCode: status,
