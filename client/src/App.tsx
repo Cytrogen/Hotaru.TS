4497bd13a1128faa7a34c67261e8a7915f44d759
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-04-28 03:44:46 -0400

    [Client] Add ProtectedRoute

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,45 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Home from "./components/Home";
+import { useAuth } from "./hooks/useAuth";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import Guard from "./components/utils/guard";
-// import PrivateMessageHomepage from "./components/private_message_homepage/Private_Message_Homepage";
-import PrivateMessageChatPage from "./components/private_message_chat_page/Private_Message_Chat_Page";
+import Home from "./components/Home";
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const { isAuthenticated, isAuthChecked } = useAuth();
+  const navigate = useNavigate();
+
+  // Redirect to login page if user is not authenticated.
+  // Redirect to home page if user is authenticated.
+  useEffect(() => {
+    if (!isAuthenticated && isAuthChecked) {
+      if (window.location.pathname === "/channels/@me") {
+        navigate("/login", { replace: true });
+      }
+    }
+  }, [isAuthenticated, isAuthChecked, navigate]);
+
+  if (!isAuthChecked) return null;
+
+  return (
+    <>
+      { isAuthenticated ? children : <Navigate to="/login" replace /> }
+    </>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <Navigate to="/channels/@me" replace /> } />
-        <Route path="/channels/@me" element={ <Guard /> }>
-          <Route path="" element={ <Home />} />
-          <Route path="dummy" element={ <PrivateMessageChatPage style={{ flex: '1 1 auto' }} /> }/>
-        </Route>
+        <Route path="/channels/@me/*" element={ <ProtectedRoute><Home /></ProtectedRoute> } />
 
         <Route path="/login" element={ <Login /> } />
         <Route path="/register" element={ <Register /> } />
