493572414c517eeab3ef40047702c7e12d74de71
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-04-28 03:45:15 -0400

    [Client] Fix frontend issue

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,15 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Route, Routes } from 'react-router-dom'
 import ServerSidebar from "./Server_Sidebar";
+import PrivateMessageHomepage from './private_message_homepage/Private_Message_Homepage'
+import PrivateMessageChatPage from './private_message_chat_page/Private_Message_Chat_Page'
 
 const Home = () => {
   return (
     <div className="vh-100 vw-100 background-color d-flex flex-row">
-      <ServerSidebar />
-      <div className="d-flex flex-row align-items-stretch" style={{ flex: '1 1 auto' }}>
-        <Outlet />
-      </div>
+      <Routes>
+        <Route path="/" element={
+          <div className="d-flex flex-row align-items-stretch flex-grow-1">
+            <ServerSidebar />
+            <PrivateMessageHomepage />
+          </div>
+        } />
+        <Route path="dummy" element={
+          <div className="d-flex flex-row align-items-stretch flex-grow-1">
+            <ServerSidebar />
+            <PrivateMessageChatPage />
+          </div>
+        } />
+      </Routes>
     </div>
   );
 }
+
 
 export default Home;
