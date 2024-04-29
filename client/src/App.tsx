86fb6baf73428bff4aa0a4f792760ea7836e1c11
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-04-29 01:45:01 -0400

    [Client] Refactor App.tsx and remove routes.tsx
    - Refactored the routing in App.tsx to directly define the routes instead of using RoutesConfig from routes.tsx.
    - Removed the routes.tsx file as its functionality has been integrated into App.tsx.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,42 @@
 import React from 'react'
-import { BrowserRouter, Routes } from 'react-router-dom'
-import { RoutesConfig } from './routes/routes'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Login from './components/Login'
+import Register from './components/Register'
+import Home from './components/Home'
+import { ProtectedRoute, UnauthenticatedRoute } from './routes/Auth_Routes'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <RoutesConfig />
+        <Route path="/" element={<Navigate to="/channels/@me" replace />} />
+        <Route
+          path="/channels/@me/*"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/login"
+          element={
+            <UnauthenticatedRoute>
+              <Login />
+            </UnauthenticatedRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <UnauthenticatedRoute>
+              <Register />
+            </UnauthenticatedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
