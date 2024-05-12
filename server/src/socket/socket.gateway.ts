5fcc5c4d95827e30a870361b7fb7d625a827ca4b
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-05-12 01:07:45 -0400

    [Feature] Add user connection tracking and message broadcasting

--- server/src/socket/socket.gateway.ts
+++ server/src/socket/socket.gateway.ts
@@ -1,6 +1,15 @@
-import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody, OnGatewayInit } from '@nestjs/websockets'
-import { Server } from 'socket.io'
+import { UseInterceptors } from '@nestjs/common'
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  MessageBody,
+  OnGatewayInit,
+  ConnectedSocket,
+} from '@nestjs/websockets'
+import { Server, Socket } from 'socket.io'
 import { SocketService } from './socket.service'
+import { SocketLoggingInterceptor } from './socket.logging.interceptor'
 import { MessagesService } from '../messages/messages.service'
 
 @WebSocketGateway(3001, {
@@ -10,6 +19,7 @@ import { MessagesService } from '../messages/messages.service'
     credentials: true,
   },
 })
+@UseInterceptors(SocketLoggingInterceptor)
 export class SocketGateway implements OnGatewayInit {
   @WebSocketServer()
   server: Server
@@ -23,8 +33,23 @@ export class SocketGateway implements OnGatewayInit {
     this.socketService.initialize(server)
   }
 
+  private userIdToSocketId = new Map<string, string>()
+
+  @SubscribeMessage('connectUser')
+  handleConnectUser(@MessageBody() userId: string, @ConnectedSocket() socket: Socket): void {
+    this.userIdToSocketId.set(userId, socket.id)
+    console.log('Connected user:', userId, '   socket id:', socket.id)
+  }
+
   @SubscribeMessage('privateMessageSent')
-  async handlePrivateMessage(@MessageBody() data: any): Promise<void> {
-    await this.messagesService.create({ senderId: data.senderId, receiverId: data.receiverId, text: data.text })
+  async handlePrivateMessage(@MessageBody() data: any, @ConnectedSocket() socket: Socket): Promise<void> {
+    const message = await this.messagesService.create({
+      senderId: data.senderId,
+      receiverId: data.receiverId,
+      text: data.text,
+    })
+    const receiverSocketId = this.userIdToSocketId.get(data.receiverId)
+    console.log('Receiver socket ID:', receiverSocketId)
+    if (receiverSocketId) socket.to(receiverSocketId).emit('newPrivateMessage', message)
   }
 }
