32cce003bd263a0e7883c208310512d1c40bb501
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-05-12 20:55:59 -0400

    [Fix] Show scrollbar when messages reached container limit

--- client/src/components/private_message_chat_page/Private_Message_Messages_Wrapper.tsx
+++ client/src/components/private_message_chat_page/Private_Message_Messages_Wrapper.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState, useContext, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { MessageContext } from '../context/Message_Context'
 import imgURL from '../../static/avatar.png'
-import PrivateMessageTextBox from './Private_Message_Text_Box'
 import { UserService, MessageService } from '../../redux/actions/serverConnection'
 import { Message, User } from '../../types/interfaces'
 import { formatDate } from '../../utils/time'
@@ -12,9 +11,6 @@ interface PrivateMessageMessagesWrapperProps {
 }
 
 type UserProfile = Pick<User, 'username' | '_id'>
-  id: string
-  content: string
-}
 
 const PrivateMessageMessagesWrapper: React.FC<PrivateMessageMessagesWrapperProps> = ({ receiverUsername }) => {
   const [messages, setMessages] = useState<Message[]>([])
@@ -22,6 +18,9 @@ const PrivateMessageMessagesWrapper: React.FC<PrivateMessageMessagesWrapperProps
   const endOfMessagesRef = useRef<null | HTMLSpanElement>(null)
   const prevMessagesLength = useRef<number>(0)
   const currentUser = useSelector((state: { auth: { user: UserProfile } }) => state.auth.user)
+
+  console.log('currentUser: ', currentUser)
+
   if (!context) {
     throw new Error('MessageContext is undefined')
   }
@@ -60,104 +59,102 @@ const PrivateMessageMessagesWrapper: React.FC<PrivateMessageMessagesWrapperProps
   }, [messages])
 
   return (
-    <div className="d-flex flex-column w-100 h-100">
-      <div style={{ flex: 1 }}>
-        <ol className="p-0 m-0 overflow-hidden" style={{ minHeight: '0', listStyle: 'none' }}>
-          {messages.map((message, index) => (
-            <li key={message.id || index} className="position-relative mx-2" style={{ outline: 'none' }}>
-              <div
-                className="position-relative"
-                style={{
-                  marginTop: '1.0625rem',
-                  minHeight: '2.75rem',
-                  paddingTop: '0.125rem',
-                  paddingBottom: '0.125rem',
-                  paddingLeft: '72px',
-                  paddingRight: '48px!important',
-                  wordWrap: 'break-word',
-                  userSelect: 'text',
-                }}>
-                <div className="position-static ms-0 ps-0" style={{ textIndent: 'none' }}>
-                  {/* User's avatar */}
-                  <img
-                    src={imgURL}
-                    className="position-absolute overflow-hidden"
-                    style={{
-                      pointerEvents: 'auto',
-                      textIndent: '-9999px',
-                      left: '16px',
-                      marginTop: 'calc(4px-0.125rem)',
-                      width: '40px',
-                      height: '40px',
-                      borderRadius: '50%',
-                      cursor: 'pointer',
-                      userSelect: 'none',
-                    }}
-                    alt=""
-                  />
-
-                  {/* Username and message time */}
-                  <h3
-                    className="overflow-hidden position-relative p-0 m-0 d-flex flex-row align-items-center"
-                    style={{
-                      display: 'block',
-                      lineHeight: '1.375rem',
-                      minHeight: '1.375rem',
-                      whiteSpace: 'break-spaces',
-                    }}>
-                    <span
-                      className="me-1 fs-6 position-relative overflow-hidden text-white"
-                      style={{
-                        fontWeight: '500',
-                        display: 'inline',
-                        verticalAlign: 'baseline',
-                        outline: 'none',
-                      }}>
-                    {message.senderId === currentUser._id ? currentUser.username : receiverUsername}
-                    </span>
-                    <span
-                      className="ms-1"
-                      style={{
-                        fontSize: '0.75rem',
-                        height: '1.25rem',
-                        verticalAlign: 'baseline',
-                        display: 'inline-block',
-                        cursor: 'default',
-                        pointerEvents: 'none',
-                        outline: 'none',
-                        fontWeight: '500',
-                        color: 'rgba(148, 154, 158)',
-                      }}>
-                    <time dateTime={message.timestamp.toString()}>{formatDate(message.timestamp)}</time>
-                    </span>
-                  </h3>
-                </div>
-
-                {/* Message Content */}
-                <div
-                  className="overflow-hidden position-relative fs-6 p-0 m-0"
+    <div
+      className="d-flex flex-column position-absolute top-0 bottom-0 overflow-y-scroll overflow-x-hidden"
+      style={{ left: 0, right: 0, overflowAnchor: 'none' }}>
+      <ol className="p-0 m-0" style={{ flex: 1, minHeight: '0', listStyle: 'none' }}>
+        {messages.map((message, index) => (
+          <li key={message.id || index} className="position-relative mx-2" style={{ outline: 'none' }}>
+            <div
+              className="position-relative"
+              style={{
+                marginTop: '1.0625rem',
+                minHeight: '2.75rem',
+                paddingTop: '0.125rem',
+                paddingBottom: '0.125rem',
+                paddingLeft: '72px',
+                paddingRight: '48px!important',
+                wordWrap: 'break-word',
+                userSelect: 'text',
+              }}>
+              <div className="position-static ms-0 ps-0" style={{ textIndent: 'none' }}>
+                {/* User's avatar */}
+                <img
+                  src={imgURL}
+                  className="position-absolute overflow-hidden"
                   style={{
-                    userSelect: 'text',
-                    whiteSpace: 'break-spaces',
-                    wordWrap: 'break-word',
-                    marginLeft: 'calc(-1 * 72px)',
-                    paddingLeft: '72px',
-                    textIndent: '0',
+                    pointerEvents: 'auto',
+                    textIndent: '-9999px',
+                    left: '16px',
+                    marginTop: 'calc(4px-0.125rem)',
+                    width: '40px',
+                    height: '40px',
+                    borderRadius: '50%',
+                    cursor: 'pointer',
+                    userSelect: 'none',
+                  }}
+                  alt=""
+                />
+
+                {/* Username and message time */}
+                <h3
+                  className="overflow-hidden position-relative p-0 m-0 d-flex flex-row align-items-center"
+                  style={{
+                    display: 'block',
                     lineHeight: '1.375rem',
-                    color: 'rgba(219, 222, 225)',
+                    minHeight: '1.375rem',
+                    whiteSpace: 'break-spaces',
                   }}>
-                  {/* TODO: Fix ESLint: `'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.(react/no-unescaped-entities) */}
-                  {/* eslint-disable-next-line react/no-unescaped-entities */}
-                <span>{message.text}</span>
-                </div>
+                  <span
+                    className="me-1 fs-6 position-relative overflow-hidden text-white"
+                    style={{
+                      fontWeight: '500',
+                      display: 'inline',
+                      verticalAlign: 'baseline',
+                      outline: 'none',
+                    }}>
+                    {message.senderId === currentUser._id ? currentUser.username : receiverUsername}
+                  </span>
+                  <span
+                    className="ms-1"
+                    style={{
+                      fontSize: '0.75rem',
+                      height: '1.25rem',
+                      verticalAlign: 'baseline',
+                      display: 'inline-block',
+                      cursor: 'default',
+                      pointerEvents: 'none',
+                      outline: 'none',
+                      fontWeight: '500',
+                      color: 'rgba(148, 154, 158)',
+                    }}>
+                    <time dateTime={message.timestamp.toString()}>{formatDate(message.timestamp)}</time>
+                  </span>
+                </h3>
               </div>
-            </li>
-          ))}
+
+              {/* Message Content */}
+              <div
+                className="overflow-hidden position-relative fs-6 p-0 m-0"
+                style={{
+                  userSelect: 'text',
+                  whiteSpace: 'break-spaces',
+                  wordWrap: 'break-word',
+                  marginLeft: 'calc(-1 * 72px)',
+                  paddingLeft: '72px',
+                  textIndent: '0',
+                  lineHeight: '1.375rem',
+                  color: 'rgba(219, 222, 225)',
+                }}>
+                {/* TODO: Fix ESLint: `'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.(react/no-unescaped-entities) */}
+                {/* eslint-disable-next-line react/no-unescaped-entities */}
+                <span>{message.text}</span>
+              </div>
+            </div>
+          </li>
+        ))}
         <span ref={endOfMessagesRef} />
-      </div>
-      <div style={{ marginTop: 'auto' }}>
-        <PrivateMessageTextBox receiverUsername={receiverUsername} />
-      </div>
+      </ol>
     </div>
   )
 }
