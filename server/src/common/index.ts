1b458f0c4537604c5c5e2d16e5a1fc3c55985681
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-05-24 16:43:15 -0400

    [Feat] Add new util function
    This function generates a random number based on a default number of 6 digits.

--- server/src/common/index.ts
+++ server/src/common/index.ts
@@ -1,1 +1,15 @@
 export const REQUEST_USER_KEY = 'user'
+
+/**
+ * Generate a random number with a specified number of digits.
+ *
+ * @param digits Default is 6
+ */
+export const generateRandomNumber = (digits: number = 6): string => {
+  let result = ''
+  for (let i = 0; i < digits; i++) {
+    const randomDigit = Math.floor(Math.random() * 10)
+    result += randomDigit
+  }
+  return result
+}
