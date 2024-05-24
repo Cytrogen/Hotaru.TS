acf94ce40d377476381a1d7400afd1e0a22a914c
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-05-24 16:43:37 -0400

    [Feat] Register a dummy user by default

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
 import * as dotenv from 'dotenv'
 import { Server } from 'socket.io'
+import { AppModule } from './app.module'
+import { RegisterUserDto } from './auth/dto'
+import { UsersService } from './users/users.service'
+import { MinIoService } from './min-io/min-io.service'
+import { AuthService } from './auth/auth.service'
 
 dotenv.config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const usersService = app.get<UsersService>(UsersService)
+  const minIoService = app.get<MinIoService>(MinIoService)
+  const authService = app.get<AuthService>(AuthService)
 
   app.enableCors({
     origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
@@ -18,6 +25,23 @@ async function bootstrap() {
 
   const server = app.getHttpServer()
   new Server(server)
+
+  const dummyUser = await usersService.findByUsername('Dummy')
+  const { url } = await minIoService.getFile('Hotaru_default_avatar.png')
+  if (!dummyUser) {
+    const dummyUserDto: RegisterUserDto = {
+      username: 'Dummy',
+      emailAddress: 'dummy@hotaru.icu',
+      password: 'dummyPassword',
+      birthYear: '2004',
+      birthMonth: 'November',
+      birthDay: '12',
+      nickname: 'Dummy',
+      avatar: url,
+    }
+    await authService.register(dummyUserDto)
+    console.log('Dummy user created.')
+  }
 
   await app.listen(process.env.PORT || 4000)
 }
